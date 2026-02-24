a8d2670df7b9be9fc6241c18e37b75e18521e519
Herlan Mistas <herlanmistas200@gmail.com>
2026-02-24 02:18:10 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -1,24 +1,26 @@
 "use client"
 
-import { useEffect, useState } from "react"
+import * as React from "react"
 
 const MOBILE_BREAKPOINT = 768
 
+const subscribe = (callback: () => void) => {
+  window.addEventListener("resize", callback)
+  return () => {
+    window.removeEventListener("resize", callback)
+  }
+}
+
+const getSnapshot = () => {
+  return window.innerWidth < MOBILE_BREAKPOINT
+}
+
+const getServerSnapshot = () => {
+  return false // Always render the desktop layout on the server
+}
+
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = useState(false)
-
-  useEffect(() => {
-    const checkIsMobile = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    }
-
-    checkIsMobile()
-    window.addEventListener("resize", checkIsMobile)
-
-    return () => {
-      window.removeEventListener("resize", checkIsMobile)
-    }
-  }, [])
-
-  return isMobile
+  // useSyncExternalStore is the recommended way to subscribe to external data sources
+  // in a way that is compatible with server rendering and avoids hydration errors.
+  return React.useSyncExternalStore(subscribe, getSnapshot, getServerSnapshot)
 }
